b9ce96b7e2e7d1692e4bbd2fcd32dfed2041bfd6
arafat717 <arafatjibon33@gmail.com>
2025-05-24 23:52:17 +0600

    added banner

--- src/Layouts/MainLayout.tsx
+++ src/Layouts/MainLayout.tsx
@@ -1,9 +1,11 @@
 import Navbar from "@/components/Navbar/Navbar";
+import ScrollToTop from "@/hooks/ScrollToTop";
 import { Outlet } from "react-router-dom";
 
 const MainLayout = () => {
   return (
     <div className="bg-[#171c35] text-white">
+      <ScrollToTop></ScrollToTop>
       <Navbar></Navbar>
       <Outlet></Outlet>
     </div>
--- src/components/Banner/Banner.tsx
+++ src/components/Banner/Banner.tsx
@@ -1,9 +1,16 @@
-import React from "react";
+import bannerOne from "../../assets/photos/banner-part-1.jpg";
 
 const Banner = () => {
   return (
-    <div>
-      <h1>this is Banner of home</h1>
+    <div className="max-w-7xl mx-auto py-5">
+      <div className="grid grid-cols-[65%_35%] justify-between gap-5">
+        <div>
+          <img src={bannerOne} alt="" />
+        </div>
+        <div className="">
+          <h1>card</h1>
+        </div>
+      </div>
     </div>
   );
 };
